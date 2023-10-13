8dc5fff0b47d2767a12bdffba8a77c0c3a4b75a6
oldgraybuzzard <k_felder@me.com>
2023-10-13 17:26:00 -0400

    works to add number and not do ones that have already been done

--- Program.cs
+++ Program.cs
@@ -9,16 +9,43 @@ string folderPath = Console.ReadLine();
 DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
 DirectoryInfo parentDirInfo = dirInfo.Parent;
 
-
 if (parentDirInfo != null && Directory.Exists(parentDirInfo.FullName))
 {
   string[] files = Directory.GetFiles(folderPath, "*.pdf", SearchOption.AllDirectories);
 
   int counter = 1;
+
+  //Read the last count from the text file
+  string counterFilePath = Path.Combine(folderPath, "counter.txt");
+  if (File.Exists(counterFilePath))
+  {
+    string counterText = File.ReadAllText(counterFilePath);
+    if (!int.TryParse(counterText, out counter))
+    {
+      Console.WriteLine("Invalid counter value in file. Starting from 1.");
+      counter = 1;
+    }
+  }
+  else
+  {
+    Console.WriteLine("Counter file does not exist. Starting from 1.");
+  }
+
+  // Code to process the pdf's
+  Regex alreadyProcessed = new Regex(@"-G\d{10}\.pdf$");
+  string logFilePath = Path.Combine(folderPath, "processed_files_log.csv");
+
   foreach (string file in files)
   {
     try
     {
+      // If file has already been processed, skip it
+      if (alreadyProcessed.IsMatch(Path.GetFileName(file)))
+      {
+        Console.WriteLine($"Skipping already processed file: {file}");
+        continue;  // Skip the rest of the loop and move to the next file
+      }
+
       // Get folder name and manipulate it
       string folderName = Path.GetFileName(Path.GetDirectoryName(file)) ?? throw new InvalidOperationException("Folder name cannot be null.");
       // Use Regular Expressions to remove the date part
@@ -41,6 +68,16 @@ if (parentDirInfo != null && Directory.Exists(parentDirInfo.FullName))
     {
       Console.WriteLine($"An error occurred while processing {file}: {ex.Message}");
     }
+  }
+
+  try
+  {
+    File.WriteAllText(counterFilePath, counter.ToString());
+    Console.WriteLine($"Counter saved to {counterFilePath}");
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine($"Failed to save counter: {ex.Message}");
   }
 
   Console.WriteLine("Process completed!");
